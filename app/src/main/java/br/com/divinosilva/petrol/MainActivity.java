e64c7a18f189903910b1b7a9099768b242fd2768
cyberlinkrv <divino_j_silva@hotmail.com>
2018-05-13 20:04:54 -0300

    CRUD - Android Studio Fire Base Funcionando 100% até aqui

--- app/src/main/java/br/com/divinosilva/petrol/MainActivity.java
+++ app/src/main/java/br/com/divinosilva/petrol/MainActivity.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import model.Pessoa;
@@ -23,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase; // inicializo os objetos do banco
     DatabaseReference databaseReference;
 
+    private List<Pessoa> listaPessoa = new ArrayList<Pessoa>(); // Para carregar Lista do banco
+    private ArrayAdapter<Pessoa> arrayAdapterPessoa;
+
+    Pessoa pessoaSelecionada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +47,52 @@ public class MainActivity extends AppCompatActivity {
         listV_dados = (ListView) findViewById(R.id.listViwerFB);
 
         inicializarFirebase();
+        eventoDatabase();//Função que traz a lista do banco
+
+        listV_dados.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                pessoaSelecionada = (Pessoa)parent.getItemAtPosition(position);
+                editNome.setText(pessoaSelecionada.getNome());
+                editEmail.setText(pessoaSelecionada.getEmail());
+            }
+        });
+    }
+
+    private void eventoDatabase() {
+        databaseReference.child("CAD_PESSOA").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                listaPessoa.clear();
+                for(DataSnapshot objetoSnapshort:dataSnapshot.getChildren()){
+                    Pessoa p = objetoSnapshort.getValue(Pessoa.class); // aqui ele tras tudo do banco
+                    listaPessoa.add(p);
+                }
+                arrayAdapterPessoa = new ArrayAdapter<Pessoa>(MainActivity.this,
+                        android.R.layout.simple_list_item_1, listaPessoa);
+                listV_dados.setAdapter(arrayAdapterPessoa);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) { // Aqui eu inflo o menu
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -61,9 +109,25 @@ public class MainActivity extends AppCompatActivity {
             p.setEmail(editEmail.getText().toString());
 
             databaseReference.child("CAD_PESSOA").child(p.getUid()).setValue(p); // Aqui preparo para enviar ao banco
-
             limparCampos();
 
+        }else
+            if(id == R.id.menu_update) {
+            Pessoa p = new Pessoa();
+
+            p.setUid(pessoaSelecionada.getUid());
+            p.setNome(editNome.getText().toString().trim());// trim tira todos espaços em branco no inicio e no final
+            p.setEmail(editEmail.getText().toString().trim());
+
+            databaseReference.child("CAD_PESSOA").child(p.getUid()).setValue(p);
+            limparCampos();
+
+        }else
+            if(id == R.id.menu_delete){
+            Pessoa p = new Pessoa();
+            p.setUid(pessoaSelecionada.getUid());
+            databaseReference.child("CAD_PESSOA").child(p.getUid()).removeValue();
+            limparCampos();
         }
 
 
